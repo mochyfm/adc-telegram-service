12d72a47980907f7137a4a280f9e0df16ebd36ee
Moisés Fernández Muiña <mochyfm@gmail.com>
2025-06-26 20:27:00 +0100

    Feature: Removed the 2FA

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from telethon import TelegramClient
+from telethon.sessions import StringSession
 from dotenv import load_dotenv
 import os
 
@@ -11,7 +12,7 @@ app = FastAPI()
 # ✅ CORS habilitado
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Reemplaza por tu dominio en producción si quieres
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -20,10 +21,11 @@ app.add_middleware(
 api_id = int(os.getenv("TELEGRAM_API_ID"))
 api_hash = os.getenv("TELEGRAM_API_HASH")
 group = os.getenv("TELEGRAM_GROUP")
+string_session = os.getenv("TELEGRAM_SESSION")
 
 @app.get("/messages")
 async def obtener_mensajes():
-    async with TelegramClient("mi_sesion", api_id, api_hash) as client:
+    async with TelegramClient(StringSession(string_session), api_id, api_hash) as client:
         mensajes = await client.get_messages(group, limit=2)
         return [
             {
@@ -35,8 +37,7 @@ async def obtener_mensajes():
             for m in mensajes
         ]
 
-# ✅ Arranque condicional
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))  # usa el puerto del entorno o 8000 por defecto
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
